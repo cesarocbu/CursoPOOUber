9496867322dce4815fcefd9e472d6c11f436c0d4
Cesar Ochoa <cesar_8ab@hotmail.com>
2022-04-26 20:15:47 -0500

    Herencia en JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("WWE1212", new Account("Cesar Ochoa", "ISI112"));
 car.passenger = 3;
 car.printDataCar();
+
+var uberX = new UberX("ALSJA", new Account("Alaks Oasas", "ALKJAA"), "Ford", "Mustang");
+uberX.passenger = 3;
+uberX.printDataCar();
